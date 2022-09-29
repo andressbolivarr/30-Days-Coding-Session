6399e1adec5f7b3a293f9b9ec9b800cf326ad111
Andy Marchan <mgiancarlos@gmail.com>
2022-09-29 17:53:45 -0400

    Day 1 JS

---
+++ JavaS/JS/VariableTest.js
@@ -0,0 +1,17 @@
+// declarar; primer paso  
+var color;
+
+// asignacion; segundo paso
+color="azul";
+var color="blanco";
+
+//Tipos de variale 
+
+// -Let y Const
+
+//Se diferencian en:
+// Declaracion, Asignacion y Scope
+
+var color="amarillo"
+let numero="2"
+numero="3";
